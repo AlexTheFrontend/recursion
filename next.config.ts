d62617c119e0ba2012d7a3a43c3c78274fee6c65
Alexander <45053518+AlexTheFrontend@users.noreply.github.com>
2025-10-20 10:42:32 +1300

    Add redirect from root to recursion

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/recursion.html',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
